858f4aa1d54e616cdd2f2dfb1a88fe5a7990ed57
Eric Courtois <eric.courtois@gmail.com>
2025-01-05 14:26:54 +0100

    tools: planner: fix coordinates in avoidance debugging window
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/avoidance/debug.py
+++ cogip/tools/planner/avoidance/debug.py
@@ -13,12 +13,13 @@ class MainWindow(QtWidgets.QWidget):
     def __init__(self, win: "DebugWindow"):
         super().__init__()
         self.win = win
-        self.scale = 0.35
-        self.margin = 400
+        self.scale = 0.30
+        self.margin = 500
+        self.offset_x = 1500
         self.offset_y = 1000
         self.setStyleSheet("background-color:white;")
         self.setWindowTitle("Avoidance")
-        self.setGeometry(QtCore.QRect(0, 500, 1500, 1000))
+        self.setGeometry(QtCore.QRect(0, 500, 1250, 900))
 
     def paintEvent(self, e):
         qp = QtGui.QPainter()
@@ -27,18 +28,21 @@ class MainWindow(QtWidgets.QWidget):
         qp.setFont(QtGui.QFont("Ubuntu", 40))
 
         if self.win.point_start:
-            qp.setPen(QtGui.QPen(QtCore.Qt.blue, 20, QtCore.Qt.SolidLine))
+            qp.setPen(QtGui.QPen(QtCore.Qt.blue, 40, QtCore.Qt.SolidLine))
             qp.drawPoint(
-                self.margin + self.win.point_start.x,
-                self.margin + (self.offset_y - self.win.point_start.y),
+                self.margin + self.offset_x - self.win.point_start.y,
+                self.margin + self.offset_y - self.win.point_start.x,
+            )
+        if self.win.point_goal:
+            qp.setPen(QtGui.QPen(QtCore.Qt.darkBlue, 40, QtCore.Qt.SolidLine))
+            qp.drawPoint(
+                self.margin + self.offset_x - self.win.point_goal.y,
+                self.margin + self.offset_y - self.win.point_goal.x,
             )
 
-        if self.win.point_goal:
-            qp.setPen(QtGui.QPen(QtCore.Qt.darkBlue, 20, QtCore.Qt.SolidLine))
-            qp.drawPoint(
-                self.margin + self.win.point_goal.x,
-                self.margin + (self.offset_y - self.win.point_goal.y),
-            )
+        # Draw 0,0 coordinates
+        qp.setPen(QtGui.QPen(QtCore.Qt.GlobalColor.green, 40))
+        qp.drawPoint(self.margin + 1500, self.margin + 1000)
 
         self.draw_border(qp)
         self.draw_obstacles(qp)
@@ -59,7 +63,7 @@ class MainWindow(QtWidgets.QWidget):
         for obstacle in chain(self.win.fixed_obstacles, self.win.dyn_obstacles):
             points = QtGui.QPolygon(
                 [
-                    QtCore.QPoint(self.margin + x, self.margin + (self.offset_y - y))
+                    QtCore.QPoint(self.margin + self.offset_x - y, self.margin + self.offset_y - x)
                     for x, y in zip(obstacle.x_list, obstacle.y_list)
                 ]
             )
@@ -69,7 +73,7 @@ class MainWindow(QtWidgets.QWidget):
 
         for obstacle in chain(self.win.fixed_obstacles, self.win.dyn_obstacles):
             cv_points = [
-                QtCore.QPoint(self.margin + x, (self.margin + self.offset_y - y))
+                QtCore.QPoint(self.margin + self.offset_x - y, self.margin + self.offset_y - x)
                 for x, y in zip(obstacle.cvx_list, obstacle.cvy_list)
             ]
             qp.drawPoints(cv_points)
@@ -77,29 +81,29 @@ class MainWindow(QtWidgets.QWidget):
     def draw_graph(self, qp: QtGui.QPainter):
         qp.setPen(QtGui.QPen(QtCore.Qt.darkGreen, 20, QtCore.Qt.SolidLine))
         for x, y in self.win.graph:
-            qp.drawPoint(self.margin + x, self.margin + (self.offset_y - y))
+            qp.drawPoint(self.margin + self.offset_x - y, self.margin + self.offset_y - x)
 
     def draw_visibility_nodes(self, qp: QtGui.QPainter):
         qp.setPen(QtGui.QPen(QtCore.Qt.red, 20, QtCore.Qt.SolidLine))
         for x, y in self.win.visibility_nodes:
-            qp.drawPoint(self.margin + x, self.margin + (self.offset_y - y))
+            qp.drawPoint(self.margin + self.offset_x - y, self.margin + self.offset_y - x)
 
     def draw_road_map(self, qp: QtGui.QPainter):
         qp.setPen(QtGui.QPen(QtCore.Qt.blue, 5, QtCore.Qt.SolidLine))
         for x1, y1, x2, y2 in self.win.road_map:
             qp.drawLine(
                 QtCore.QLine(
-                    self.margin + x1,
-                    self.margin + (self.offset_y - y1),
-                    self.margin + x2,
-                    self.margin + (self.offset_y - y2),
+                    self.margin + self.offset_x - y1,
+                    self.margin + self.offset_y - x1,
+                    self.margin + self.offset_x - y2,
+                    self.margin + self.offset_y - x2,
                 )
             )
 
     def draw_path(self, qp: QtGui.QPainter):
         qp.setPen(QtGui.QPen(QtCore.Qt.red, 10, QtCore.Qt.SolidLine))
         points = QtGui.QPolygon(
-            [QtCore.QPoint(self.margin + x, self.margin + (self.offset_y - y)) for x, y in self.win.path]
+            [QtCore.QPoint(self.margin + self.offset_x - y, self.margin + self.offset_y - x) for x, y in self.win.path]
         )
         qp.drawPolyline(points)
 
