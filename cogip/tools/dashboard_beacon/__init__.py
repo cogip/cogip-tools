5ea90ea9b4a072b56cc2bfd195cc9d3ea7b5a701
Eric Courtois <eric.courtois@gmail.com>
2025-01-05 16:49:35 +0100

    tools: remove dashboard-beacon and use dashboard on beacon and robots
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/dashboard_beacon/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,  # logging.DEBUG
-    format="[%(asctime)s][%(name)s][%(threadName)s] %(levelname)s: %(message)s",
-)
-
-logger = logging.getLogger(__name__)
