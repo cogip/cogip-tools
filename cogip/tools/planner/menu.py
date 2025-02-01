a34e08a026824c3cc6bbceda8002f4c49cc0c032
Eric Courtois <eric.courtois@gmail.com>
2025-02-01 23:24:57 +0100

    WIP: tools: planner: add controls for SC Servos
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/menu.py
+++ cogip/tools/planner/menu.py
@@ -47,12 +47,61 @@ cameras_menu = models.ShellMenu(
     ],
 )
 
-robot_actuators_commands = []
+robot_actuators_commands = [
+    "magnet_side_right_in",
+    "magnet_side_right_center",
+    "magnet_side_right_out",
+    "arm_right_front",
+    "arm_right_center",
+    "arm_right_side",
+    "magnet_center_right_out",
+    "magnet_center_right_center",
+    "magnet_center_right_in",
+    "magnet_center_left_out",
+    "magnet_center_left_center",
+    "magnet_center_left_in",
+    "arm_left_front",
+    "arm_left_center",
+    "arm_left_side",
+    "magnet_side_left_in",
+    "magnet_side_left_center",
+    "magnet_side_left_out",
+    "arm_grip_left_open",
+    "arm_grip_left_close",
+    "arm_grip_left_hold",
+    "arm_grip_right_open",
+    "arm_grip_right_close",
+    "arm_grip_right_hold",
+    "grip_right_open",
+    "grip_right_close1",
+    "grip_right_close2",
+    "grip_left_open",
+    "grip_left_close1",
+    "grip_left_close2",
+]
 
 robot_actuators_menu = models.ShellMenu(
     name="Actuators",
     entries=[
         models.MenuEntry(cmd=f"act_{cmd}", desc=f"{cmd.replace('_', ' ').title()}") for cmd in robot_actuators_commands
+    ],
+)
+
+robot_actuators_multi_commands = [
+    "tribune_grab",
+    "tribune_spread",
+    "arms_open",
+    "arms_hold1",
+    "arms_hold2",
+    "arms_release",
+    "arms_close",
+]
+
+robot_actuators_multi_menu = models.ShellMenu(
+    name="Actuators Multi",
+    entries=[
+        models.MenuEntry(cmd=f"act_{cmd}", desc=f"{cmd.replace('_', ' ').title()}")
+        for cmd in robot_actuators_multi_commands
     ],
 )
 
