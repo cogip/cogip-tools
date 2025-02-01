f1705f755c5375878058ec3f026183848ce8e0e6
Eric Courtois <eric.courtois@gmail.com>
2025-02-01 23:15:54 +0100

    WIP: tools: planner: add unit test strategy to grab tribune
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/actions/base_actions.py
+++ cogip/tools/planner/actions/base_actions.py
@@ -1,6 +1,8 @@
+import asyncio
 from typing import TYPE_CHECKING
 
 from cogip import models
+from cogip.models import artifacts
 from cogip.tools.planner.actions.actions import Action, Actions
 from cogip.tools.planner.avoidance.avoidance import AvoidanceStrategy
 from cogip.tools.planner.camp import Camp
@@ -163,3 +165,127 @@ class ParkingAction(Action):
         await self.planner.sio_ns.emit("score", self.game_context.score)
         await self.planner.sio_ns.emit("robot_end")
         self.actions.clear()
+
+
+class GrabTribuneAction(Action):
+    """
+    Action used to grab tribune using magnets.
+    """
+
+    def __init__(self, planner: "Planner", actions: Actions, tribune_id: artifacts.TribuneID):
+        super().__init__("GrabTribune action", planner, actions)
+        self.before_action_func = self.before_action
+        self.tribune = self.game_context.tribunes[tribune_id]
+        self.shift_approach = 335
+        self.shift_forward = 160
+
+        match self.tribune.O:
+            case -90:
+                self.approach_x = self.tribune.x
+                self.approach_y = -1500 + self.shift_approach
+                self.capture_x = self.approach_x
+                self.capture_y = self.approach_y - self.shift_forward
+            case 90:
+                self.approach_x = self.tribune.x
+                self.approach_y = 1500 - self.shift_approach
+                self.capture_x = self.approach_x
+                self.capture_y = self.approach_y + self.shift_forward
+            case 180:
+                self.approach_x = -1000 + self.shift_approach
+                self.approach_y = self.tribune.y
+                self.capture_x = self.approach_x - self.shift_forward
+                self.capture_y = self.approach_y
+
+    async def recycle(self):
+        # await actuators.cart_magnet_off(self.planner)
+        # await actuators.cart_in(self.planner)
+        self.tribune.enabled = True
+        self.recycled = True
+
+    async def before_action(self):
+        self.start_pose = models.Pose(
+            x=self.planner.pose_current.x,
+            y=self.planner.pose_current.y,
+            O=self.planner.pose_current.angle,
+        )
+
+        pose = Pose(
+            x=self.approach_x,
+            y=self.approach_y,
+            O=self.tribune.O,
+            max_speed_linear=66,
+            max_speed_angular=66,
+            allow_reverse=False,
+            before_pose_func=self.before_pose1,
+            after_pose_func=self.after_pose1,
+        )
+        self.poses.append(pose)
+
+        # Capture
+        pose = Pose(
+            x=self.capture_x,
+            y=self.capture_y,
+            O=self.tribune.O,
+            max_speed_linear=5,
+            max_speed_angular=5,
+            allow_reverse=False,
+            bypass_anti_blocking=True,
+            timeout_ms=3000,
+            after_pose_func=self.after_pose2,
+        )
+        self.poses.append(pose)
+
+        # Step-back
+        pose = Pose(
+            x=self.approach_x,
+            y=self.approach_y,
+            O=self.tribune.O,
+            max_speed_linear=10,
+            max_speed_angular=10,
+            allow_reverse=True,
+            after_pose_func=self.after_pose3,
+        )
+        self.poses.append(pose)
+
+    async def before_pose1(self):
+        # await asyncio.gather(
+        #     actuators.bottom_grip_close(self.planner),
+        #     actuators.top_grip_close(self.planner),
+        # )
+        # await asyncio.gather(
+        #     actuators.top_lift_up(self.planner),
+        #     actuators.bottom_lift_up(self.planner),
+        # )
+        pass
+
+    async def after_pose1(self):
+        # await actuators.cart_out(self.planner)
+        # await actuators.cart_magnet_on(self.planner)
+
+        self.tribune.enabled = False
+
+    async def after_pose2(self):
+        await asyncio.sleep(1)
+
+    async def after_pose3(self):
+        self.tribune.enabled = True
+        # if BoolSensorEnum.MAGNET_LEFT in self.game_context.emulated_actuator_states:
+        #     self.game_context.bool_sensor_states[BoolSensorEnum.MAGNET_LEFT].state = True
+        # if BoolSensorEnum.MAGNET_RIGHT in self.game_context.emulated_actuator_states:
+        #     self.game_context.bool_sensor_states[BoolSensorEnum.MAGNET_RIGHT].state = True
+
+    def weight(self) -> float:
+        if not self.tribune.enabled:
+            return 0
+        # if not (
+        #     self.game_context.bool_sensor_states[BoolSensorEnum.BOTTOM_GRIP_LEFT].state
+        #     and self.game_context.bool_sensor_states[BoolSensorEnum.BOTTOM_GRIP_RIGHT].state
+        # ):
+        #     return 0
+        # if (
+        #     self.game_context.bool_sensor_states[BoolSensorEnum.MAGNET_LEFT].state
+        #     or self.game_context.bool_sensor_states[BoolSensorEnum.MAGNET_RIGHT].state
+        # ):
+        #     return 0
+
+        return 2000000.0
