96fd9ffd881b31bd18c678207fd2bc8492b0bb7a
Eric Courtois <eric.courtois@gmail.com>
2025-04-28 19:26:24 +0200

    tools: planner: add controls for new positional actuator (lift)
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/actuators.py
+++ cogip/tools/planner/actuators.py
@@ -16,8 +16,38 @@ async def positional_motor_command(
     planner: "Planner",
     motor: PositionalActuatorEnum,
     command: int,
+    speed: int = 100,
+    timeout: int = 2000,
 ) -> float:
-    await planner.sio_ns.emit("actuator_command", PositionalActuatorCommand(id=motor, command=command).model_dump())
+    await planner.sio_ns.emit(
+        "actuator_command",
+        PositionalActuatorCommand(
+            id=motor,
+            command=command,
+            speed=speed,
+            timeout=timeout,
+        ).model_dump(),
+    )
+    return 0
+
+
+async def lift_0(planner: "Planner", speed: int = 100) -> float:
+    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_LIFT, 0, speed)
+    return 0
+
+
+async def lift_5(planner: "Planner", speed: int = 100) -> float:
+    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_LIFT, 5, speed)
+    return 0
+
+
+async def lift_125(planner: "Planner", speed: int = 100) -> float:
+    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_LIFT, 125, speed)
+    return 0
+
+
+async def lift_140(planner: "Planner", speed: int = 100) -> float:
+    await positional_motor_command(planner, PositionalActuatorEnum.MOTOR_LIFT, 140, speed)
     return 0
 
 
