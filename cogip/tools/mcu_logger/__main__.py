da73509a790d0bf00cc99eef770024103a8d98be
Eric Courtois <eric.courtois@gmail.com>
2025-05-10 10:01:04 +0200

    raspios: add a service to log outputs from mcu-firmware
    Like in other services, this logger will output the messages emitted
    by mcu-firmware on the serial port in console, log file and syslog.
    This allows to read all the logs at the same place and with timestamp.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

---
+++ cogip/tools/mcu_logger/__main__.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from typing import Annotated
+
+import serial
+import typer
+
+from . import logger
+
+
+def main_opt(
+    port: Annotated[
+        Path,
+        typer.Option(
+            "-p",
+            "--port",
+            help="Serial port connected to the STM32 running mcu-firmware",
+            envvar="MCU_LOGGER_PORT",
+        ),
+    ],
+    baud_rate: Annotated[
+        int,
+        typer.Option(
+            "-b",
+            "--baud-rate",
+            help="Baud rate.",
+            envvar="MCU_LOGGER_BAUD_RATE",
+        ),
+    ] = 115200,
+):
+    ser = serial.Serial(str(port), baud_rate)
+
+    try:
+        while True:
+            if ser.in_waiting > 0:
+                line = ser.readline().decode("utf-8").strip()
+                logger.info(line)
+    except KeyboardInterrupt:
+        return
+
+
+def main():
+    """
+    This tools outputs from a serial port, like mcu-firmware outputs, and forward them to the logger.
+
+    During installation of cogip-tools, a script called `cogip-mcu-logger`
+    will be created using this function as entrypoint.
+    """
+    typer.run(main_opt)
+
+
+if __name__ == "__main__":
+    main()
