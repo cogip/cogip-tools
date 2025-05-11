f6e4026b6755a56506056910707b45a07fb8ab4d
Eric Courtois <eric.courtois@gmail.com>
2025-05-11 20:05:18 +0200

    protobuf: update links to Protobuf messages and remove dead links
    PB_Actuator has moved and been updated.
    PB_Pid has moved.
    PB_Menu and PB_Command have been removed.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/protobuf/PB_Actuators_pb2.pyi
+++ cogip/protobuf/PB_Actuators_pb2.pyi
@@ -3,46 +3,29 @@ from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
-ANALOGSERVO_BOTTOM_GRIP_LEFT: PB_PositionalActuatorEnum
-ANALOGSERVO_BOTTOM_GRIP_RIGHT: PB_PositionalActuatorEnum
-ANALOGSERVO_PAMI: PB_PositionalActuatorEnum
-ANALOGSERVO_TOP_GRIP_LEFT: PB_PositionalActuatorEnum
-ANALOGSERVO_TOP_GRIP_RIGHT: PB_PositionalActuatorEnum
+BLOCKED: PB_PositionalActuatorStateEnum
 BOOL_SENSOR: PB_ActuatorsTypeEnum
-BOTTOM_GRIP_LEFT: PB_BoolSensorEnum
-BOTTOM_GRIP_RIGHT: PB_BoolSensorEnum
-CART_MAGNET_LEFT: PB_PositionalActuatorEnum
-CART_MAGNET_RIGHT: PB_PositionalActuatorEnum
 DESCRIPTOR: _descriptor.FileDescriptor
-LXSERVO_ARM_PANEL: PB_ServoEnum
-LXSERVO_LEFT_CART: PB_ServoEnum
-LXSERVO_RIGHT_CART: PB_ServoEnum
-MAGNET_LEFT: PB_BoolSensorEnum
-MAGNET_RIGHT: PB_BoolSensorEnum
-MOTOR_BOTTOM_LIFT: PB_PositionalActuatorEnum
-MOTOR_TOP_LIFT: PB_PositionalActuatorEnum
+INTERMEDIATE_REACHED: PB_PositionalActuatorStateEnum
+MOTOR_LIFT: PB_PositionalActuatorEnum
+NONE: PB_BoolSensorEnum
 POSITIONAL: PB_ActuatorsTypeEnum
-SERVO: PB_ActuatorsTypeEnum
-TOP_GRIP_LEFT: PB_BoolSensorEnum
-TOP_GRIP_RIGHT: PB_BoolSensorEnum
+REACHED: PB_PositionalActuatorStateEnum
+TIMEOUT: PB_PositionalActuatorStateEnum
 
 class PB_ActuatorCommand(_message.Message):
-    __slots__ = ["positional_actuator", "servo"]
+    __slots__ = ["positional_actuator"]
     POSITIONAL_ACTUATOR_FIELD_NUMBER: _ClassVar[int]
-    SERVO_FIELD_NUMBER: _ClassVar[int]
     positional_actuator: PB_PositionalActuatorCommand
-    servo: PB_ServoCommand
-    def __init__(self, servo: _Optional[_Union[PB_ServoCommand, _Mapping]] = ..., positional_actuator: _Optional[_Union[PB_PositionalActuatorCommand, _Mapping]] = ...) -> None: ...
+    def __init__(self, positional_actuator: _Optional[_Union[PB_PositionalActuatorCommand, _Mapping]] = ...) -> None: ...
 
 class PB_ActuatorState(_message.Message):
-    __slots__ = ["bool_sensor", "positional_actuator", "servo"]
+    __slots__ = ["bool_sensor", "positional_actuator"]
     BOOL_SENSOR_FIELD_NUMBER: _ClassVar[int]
     POSITIONAL_ACTUATOR_FIELD_NUMBER: _ClassVar[int]
-    SERVO_FIELD_NUMBER: _ClassVar[int]
     bool_sensor: PB_BoolSensor
     positional_actuator: PB_PositionalActuator
-    servo: PB_Servo
-    def __init__(self, servo: _Optional[_Union[PB_Servo, _Mapping]] = ..., positional_actuator: _Optional[_Union[PB_PositionalActuator, _Mapping]] = ..., bool_sensor: _Optional[_Union[PB_BoolSensor, _Mapping]] = ...) -> None: ...
+    def __init__(self, positional_actuator: _Optional[_Union[PB_PositionalActuator, _Mapping]] = ..., bool_sensor: _Optional[_Union[PB_BoolSensor, _Mapping]] = ...) -> None: ...
 
 class PB_BoolSensor(_message.Message):
     __slots__ = ["id", "state"]
@@ -53,53 +36,35 @@ class PB_BoolSensor(_message.Message):
     def __init__(self, id: _Optional[_Union[PB_BoolSensorEnum, str]] = ..., state: bool = ...) -> None: ...
 
 class PB_PositionalActuator(_message.Message):
-    __slots__ = ["command", "id", "is_blocked"]
-    COMMAND_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ["id", "position", "state"]
     ID_FIELD_NUMBER: _ClassVar[int]
-    IS_BLOCKED_FIELD_NUMBER: _ClassVar[int]
-    command: int
+    POSITION_FIELD_NUMBER: _ClassVar[int]
+    STATE_FIELD_NUMBER: _ClassVar[int]
     id: PB_PositionalActuatorEnum
-    is_blocked: bool
-    def __init__(self, is_blocked: bool = ..., id: _Optional[_Union[PB_PositionalActuatorEnum, str]] = ..., command: _Optional[int] = ...) -> None: ...
+    position: int
+    state: PB_PositionalActuatorStateEnum
+    def __init__(self, id: _Optional[_Union[PB_PositionalActuatorEnum, str]] = ..., state: _Optional[_Union[PB_PositionalActuatorStateEnum, str]] = ..., position: _Optional[int] = ...) -> None: ...
 
 class PB_PositionalActuatorCommand(_message.Message):
-    __slots__ = ["command", "id"]
+    __slots__ = ["command", "id", "speed", "timeout"]
     COMMAND_FIELD_NUMBER: _ClassVar[int]
     ID_FIELD_NUMBER: _ClassVar[int]
+    SPEED_FIELD_NUMBER: _ClassVar[int]
+    TIMEOUT_FIELD_NUMBER: _ClassVar[int]
     command: int
     id: PB_PositionalActuatorEnum
-    def __init__(self, id: _Optional[_Union[PB_PositionalActuatorEnum, str]] = ..., command: _Optional[int] = ...) -> None: ...
-
-class PB_Servo(_message.Message):
-    __slots__ = ["command", "id", "is_blocked", "position"]
-    COMMAND_FIELD_NUMBER: _ClassVar[int]
-    ID_FIELD_NUMBER: _ClassVar[int]
-    IS_BLOCKED_FIELD_NUMBER: _ClassVar[int]
-    POSITION_FIELD_NUMBER: _ClassVar[int]
-    command: int
-    id: PB_ServoEnum
-    is_blocked: bool
-    position: int
-    def __init__(self, is_blocked: bool = ..., id: _Optional[_Union[PB_ServoEnum, str]] = ..., position: _Optional[int] = ..., command: _Optional[int] = ...) -> None: ...
-
-class PB_ServoCommand(_message.Message):
-    __slots__ = ["command", "id", "time"]
-    COMMAND_FIELD_NUMBER: _ClassVar[int]
-    ID_FIELD_NUMBER: _ClassVar[int]
-    TIME_FIELD_NUMBER: _ClassVar[int]
-    command: int
-    id: PB_ServoEnum
-    time: int
-    def __init__(self, id: _Optional[_Union[PB_ServoEnum, str]] = ..., command: _Optional[int] = ..., time: _Optional[int] = ...) -> None: ...
+    speed: int
+    timeout: int
+    def __init__(self, id: _Optional[_Union[PB_PositionalActuatorEnum, str]] = ..., command: _Optional[int] = ..., speed: _Optional[int] = ..., timeout: _Optional[int] = ...) -> None: ...
 
 class PB_ActuatorsTypeEnum(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
-    __slots__ = []
-
-class PB_ServoEnum(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = []
 
 class PB_PositionalActuatorEnum(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = []
 
 class PB_BoolSensorEnum(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
+
+class PB_PositionalActuatorStateEnum(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = []
