525155587f1907958ae367a70ca8c09f7d3eb94c
Eric Courtois <eric.courtois@gmail.com>
2025-05-28 14:36:02 +0200

    raspios: disable chromium cache and unwanted features
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- raspios/overlay-rootfs/root/start_chromium.sh
+++ raspios/overlay-rootfs/root/start_chromium.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-/usr/bin/chromium-browser http://localhost:808ROBOT_ID \
+/usr/bin/chromium-browser http://localhost:8080 \
     --no-sandbox \
     --disable-setuid-sandbox \
     --enable-features=UseOzonePlatform \
     --ozone-platform=wayland \
     --kiosk \
+    --incognito \
     --disable-infobars \
     --disable-extensions \
     --disable-pinch \
+    --disable-translate \
+    --disable-features=TranslateUI \
+    --overscroll-history-navigation=0 \
+    --noerrdialogs \
+    --disk-cache-dir=/dev/null \
     --remote-debugging-port=9222
