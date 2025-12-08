8d1625bc4c01dd54b107dd6358a56e90da4e957a
Eric Courtois <eric.courtois@gmail.com>
2025-12-08 19:49:41 +0100

    tools: camera: migrate from v4l2py to linuxpy.video
    v4l2py has been deprecated in favor of linuxpy.
    
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/camera/info.py
+++ cogip/tools/camera/info.py
@@ -1,10 +1,11 @@
 import logging
 from pathlib import Path
+from time import sleep
 from typing import Annotated, Optional
 
 import cv2
 import typer
-import v4l2py
+from linuxpy.video import device
 
 from . import logger
 from .arguments import CameraName, VideoCodec
@@ -46,63 +47,62 @@ def cmd_info(
     debug = obj.get("debug", False)
 
     if debug:
-        v4l2py.device.log.setLevel(logging.DEBUG)
+        logging.getLogger("linuxpy.video").setLevel(logging.DEBUG)
     else:
-        v4l2py.device.log.setLevel(logging.INFO)
+        logging.getLogger("linuxpy.video").setLevel(logging.INFO)
 
     if camera_name:
         if not Path(camera_name.val).exists():
             logger.error(f"Camera not found: {camera_name}")
             return
-        device = v4l2py.Device(camera_name.val)
+        camera = device.Device(camera_name.val)
         try:
-            device.open()
+            camera.open()
         except OSError:
             logger.error(f"Failed to open {camera_name.val}")
             return
-        print_device_info(device)
-        device.close()
+        print_device_info(camera)
+        camera.close()
         show_stream(camera_name, camera_codec, camera_width, camera_height)
         return
 
-    for device in v4l2py.iter_video_capture_devices():
+    for camera in device.iter_video_capture_devices():
         try:
-            device.open()
+            camera.open()
         except OSError:
             pass
         else:
-            print_device_info(device)
-            device.close()
+            print_device_info(camera)
+            camera.close()
             print()
 
 
-def print_device_info(device: v4l2py.device.Device):
-    logger.info(f"Camera: {device.filename} ({device.info.card})")
-
-    if not device.info:
-        logger.warning("  - No device info")
-        return
+def print_device_info(camera: device.Device):
+    logger.info(f"Camera: {camera.filename} ({getattr(camera.info, 'card', 'N/A')})")
 
     logger.info("  - Frame sizes:")
-    frame_sizes: set[tuple[v4l2py.device.PixelFormat, int, int]] = set()
-    for frame_size in device.info.frame_sizes:
-        frame_size: v4l2py.device.FrameType
+    frame_sizes: set[tuple[device.PixelFormat, int, int]] = set()
+    for frame_size in camera.info.frame_sizes:
+        frame_size: device.FrameType
         frame_sizes.add((frame_size.pixel_format, frame_size.width, frame_size.height))
 
     for pixel_format, width, height in sorted(frame_sizes):
         logger.info(f"    - {pixel_format.name:>5s} - {width:4d} x {height:4d}")
 
     logger.info("  - Available controls:")
-    for control in device.controls.values():
+    for control in camera.controls.values():
         match type(control):
-            case v4l2py.device.BooleanControl:
+            case device.BooleanControl:
+                control: device.BooleanControl
                 logger.info(f"    - {control.name} - value={control.value} - default={control.default}")
-            case v4l2py.device.IntegerControl:
+            case device.IntegerControl:
+                control: device.IntegerControl
                 logger.info(
                     f"    - {control.name} - value={control.value} - default={control.default}"
                     f" - min={control.minimum} - max={control.maximum}"
                 )
-            case v4l2py.device.MenuControl:
+            case device.MenuControl:
+                control: device.MenuControl
                 logger.info(f"    - {control.name} - value={control.value} - default={control.default}")
                 for key, value in control.data.items():
                     logger.info(f"      - {key}: {value}")
@@ -129,15 +129,21 @@ def show_stream(name: CameraName, codec: VideoCodec, width: int, height: int):
     if not ret:
         logger.warning(f"Frame height {height} not supported")
 
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_EXPANDED)
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     while True:
         ret, frame = cap.read()
-        if not ret:
+        if not ret or frame is None:
             logger.warning("Cannot read current frame.")
+            sleep(0.2)
+            continue
 
         cv2.imshow(window_name, frame)
 
         k = cv2.waitKey(1)
         if k == exit_key:
             break
+
+    cap.release()
+    cv2.destroyAllWindows()
