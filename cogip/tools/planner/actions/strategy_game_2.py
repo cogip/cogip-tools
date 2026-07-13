0af904b6fbc0a8b5a80f01c93f9f1bd062422d57
Eric Courtois <eric.courtois@gmail.com>
2026-07-13 17:02:05 +0200

    tools: planner: add approval and game strategies
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

---
+++ cogip/tools/planner/actions/strategy_game_2.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING
+
+from cogip.models.artifacts import CollectionAreaID, PantryID
+from cogip.tools.planner.actions.action_align import AlignTopCornerAction
+from cogip.tools.planner.actions.action_parking import ParkingAction, RunAwayAction
+from cogip.tools.planner.actions.capture_crates import CaptureCratesAction
+from cogip.tools.planner.actions.cursor import CursorAction
+from cogip.tools.planner.actions.drop_crates import DropCratesAction
+from cogip.tools.planner.actions.push_granary_crates import PushGranaryCratesAction
+from cogip.tools.planner.actions.strategy import Strategy
+from cogip.tools.planner.pose import AdaptedPose
+
+if TYPE_CHECKING:
+    from ..planner import Planner
+
+
+class Game2Strategy(Strategy):
+    def __init__(self, planner: "Planner"):
+        super().__init__(planner)
+        self.can_wait = True
+
+        self.append(CursorAction(planner, self, 10_000_000.0, unit_test=False))
+        self.append(PushGranaryCratesAction(planner, self, weight=9_000_000.0, unit_test=False))
+
+        self.append(CaptureCratesAction(planner, self, CollectionAreaID.LocalTopSide, next(self.priority)))
+        self.append(DropCratesAction(planner, self, PantryID.LocalSide, next(self.priority)))
+
+        self.append(CaptureCratesAction(planner, self, CollectionAreaID.LocalBottomSide, next(self.priority)))
+        self.append(DropCratesAction(planner, self, PantryID.LocalBottom, next(self.priority)))
+
+        self.append(CaptureCratesAction(planner, self, CollectionAreaID.LocalCenter, next(self.priority)))
+        self.append(CaptureCratesAction(planner, self, CollectionAreaID.LocalBottom, next(self.priority)))
+
+        self.append(DropCratesAction(planner, self, PantryID.MiddleBottom, next(self.priority)))
+        self.append(DropCratesAction(planner, self, PantryID.MiddleCenter, next(self.priority)))
+
+        self.append(RunAwayAction(planner, self))
+        self.append(ParkingAction(planner, self, AdaptedPose(x=740, y=-1205, O=0).pose))
+
+
+class Game2AlignStrategy(Game2Strategy):
+    def __init__(self, planner: "Planner"):
+        super().__init__(planner)
+        self.insert(0, AlignTopCornerAction(planner, self, weight=3_000_000.0))
