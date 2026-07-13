012967866ce2756906a29aa499472100b8d8579e
Eric Courtois <eric.courtois@gmail.com>
2026-07-13 16:52:09 +0200

    tools: planner: context: update artifacts initialization
    Signed-off-by: Eric Courtois <eric.courtois@gmail.com>

--- cogip/tools/planner/context.py
+++ cogip/tools/planner/context.py
@@ -82,6 +82,27 @@ class GameContext:
         new_ctx.pantries = {k: v.model_copy() for k, v in self.pantries.items()}
         return new_ctx
 
+    def create_pantry(self, pantry_id: PantryID):
+        x, y, angle, training = pantries[pantry_id]
+        enabled = (
+            self.shared_properties.table == TableEnum.Game
+            or (self.shared_properties.table == TableEnum.Training and training)
+            or self.shared_properties.robot_id >= 2
+        )
+        pose = AdaptedPose(x=x, y=y, O=angle)
+        if angle is None:
+            self.pantries[pantry_id] = Pantry(
+                **pose.model_dump(include={"x", "y"}),
+                id=pantry_id,
+                enabled=enabled,
+            )
+        else:
+            self.pantries[pantry_id] = Pantry(
+                **pose.model_dump(include={"x", "y", "O"}),
+                id=pantry_id,
+                enabled=enabled,
+            )
+
     def create_artifacts(self):
         self.collection_areas = {}
         for collection_area_id, values in collection_areas.items():
@@ -94,37 +115,22 @@ class GameContext:
                 self.collection_areas[collection_area_id] = CollectionArea(
                     **pose.model_dump(include={"x", "y"}),
                     id=collection_area_id,
-                    enabled=enabled,
+                    enabled=enabled and self.shared_properties.robot_id == 1,
                 )
             else:
                 self.collection_areas[collection_area_id] = CollectionArea(
                     **pose.model_dump(include={"x", "y", "O"}),
                     id=collection_area_id,
-                    enabled=enabled,
+                    enabled=enabled and self.shared_properties.robot_id == 1,
                 )
 
         self.pantries = {}
-        for pantry_id, values in pantries.items():
-            x, y, angle, training = values
-            enabled = self.shared_properties.table == TableEnum.Game or (
-                self.shared_properties.table == TableEnum.Training and training
-            )
-            pose = AdaptedPose(x=x, y=y, O=angle)
-            if angle is None:
-                self.pantries[pantry_id] = Pantry(
-                    **pose.model_dump(include={"x", "y"}),
-                    id=pantry_id,
-                    enabled=enabled,
-                )
-            else:
-                self.pantries[pantry_id] = Pantry(
-                    **pose.model_dump(include={"x", "y", "O"}),
-                    id=pantry_id,
-                    enabled=enabled,
-                )
+        for pantry_id in pantries.keys():
+            self.create_pantry(pantry_id)
 
         # We can consider that these pantries won't be used by the opponent robot
         if self.shared_properties.robot_id == 1:
+            self.pantries[PantryID.LocalTop].enabled = False
             self.pantries[PantryID.LocalSide].enabled = False
             self.pantries[PantryID.LocalCenter].enabled = False
             self.pantries[PantryID.LocalBottom].enabled = False
@@ -180,7 +186,7 @@ class GameContext:
 
         # Crates from granary
         self.fixed_obstacles[FixedObstacleID.CratesFromGranary] = FixedObstacle(
-            **AdaptedPose(x=475, y=-800).model_dump(include={"x", "y"}),
+            **AdaptedPose(x=475, y=-700).model_dump(include={"x", "y"}),
             length=150,
             width=200,
             id=FixedObstacleID.CratesFromGranary,
